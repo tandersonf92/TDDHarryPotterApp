6a3940c3b38b424636d02b538a1ab65c1a6a9042
tandersonf <anderson.oliveira@cora.com.br>
2024-02-23 09:40:22 -0300

    update Data Domain with new protocol function

--- Data/RemoteDataSource.swift
+++ Data/RemoteDataSource.swift
@@ -1,6 +1,7 @@
 import DomainInterfaces
 
 final class RemoteDataSource: DataRepositoryProtocol {
-    func getHogwartsCharacterList(completion: @escaping (([HogwartsCharacterModel]) -> Void), failure: @escaping (() -> Void)) {
-    }
+    func getAllCharactersList(completion: @escaping (([HogwartsCharacterModel]) -> Void), failure: @escaping (() -> Void)) {}
+    
+    func getStudentsList(completion: @escaping (([HogwartsCharacterModel]) -> Void), failure: @escaping (() -> Void)) {}
 }
