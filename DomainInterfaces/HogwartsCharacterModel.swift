50d89fca1e941df9cb9b90e4e93da7de8e9755aa
tandersonf <anderson.oliveira@cora.com.br>
2024-03-05 16:39:31 -0300

    adiciona init public no model

--- DomainInterfaces/HogwartsCharacterModel.swift
+++ DomainInterfaces/HogwartsCharacterModel.swift
@@ -5,4 +5,13 @@ public struct HogwartsCharacterModel: Decodable {
     public var house: String
     public var wizard: Bool
     public var image: String?
+
+    public init(name: String, species: String, gender: String, house: String, wizard: Bool, image: String? = nil) {
+        self.name = name
+        self.species = species
+        self.gender = gender
+        self.house = house
+        self.wizard = wizard
+        self.image = image
+    }
 }
