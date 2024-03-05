8befe2deec896f7f6817e7a7b361a85955dd5282
tandersonf <anderson.oliveira@cora.com.br>
2024-03-05 16:54:06 -0300

    create infra layer

---
+++ Infra/Infra.h
@@ -0,0 +1,18 @@
+//
+//  Infra.h
+//  Infra
+//
+//  Created by Cora on 05/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
